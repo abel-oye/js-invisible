7fa368af52aca1e3eb4119ceb02950ebe5c6cca8
abel <abel@abeldeMacBook-Pro-2.local>
2021-08-06 21:50:18 +0800

    add output

--- test/test.js
+++ test/test.js
@@ -1,5 +1,9 @@
 var ji = require('../');
+var fs = require('fs');
+var path = require('path')
 
+var str = "abc"
 
-console.log('(',ji.encode("console.log(2222)"),')')
-console.log(ji.deconde(ji.encode("console.log(2222)")))
+console.log('(',ji.encode(str),')')
+fs.writeFileSync(path.join(__dirname,"./output"), ji.encode(str))
+// console.log(ji.deconde())
